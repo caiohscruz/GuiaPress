a7a85dbec19de3b7c66ccea9654c70815cdaf062
caiohscruz <caiohscruz@hotmail.com>
2021-09-02 19:48:18 -0300

    Add listagem de categorias

--- src/categories/CategoriesController.js
+++ src/categories/CategoriesController.js
@@ -5,28 +5,42 @@ const Category = require("./Category")
 
 const slugify = require("slugify")
 
-router.get("/categories", (req,res) => {
+router.get("/categories", (req, res) => {
     res.send("teste")
 })
 
 // Form para criar categories
-router.get("/admin/categories/new", (req,res) => {
+router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/newCategory.ejs")
 })
 
 // Salvar categories
-router.post("/categories/save", (req,res) => {
+router.post("/categories/save", (req, res) => {
     var title = req.body.title
-    if(title != undefined){
+    if (title != undefined) {
         Category.create({
             title: title,
             slug: slugify(title)
-        }).then(()=> {
+        }).then(() => {
             res.redirect("/")
         })
-    }else{
+    } else {
         res.redirect("admin/categories/newCategory.ejs")
     }
+})
+
+// Form para listar categories
+router.get("/admin/categories/", async (req, res) => {
+    await Category.findAll({
+        raw: true,
+        order: [
+            ['id', 'ASC']
+        ]
+    }).then(categories => {
+        res.render("admin/categories/indexCategory.ejs", {
+            categories: categories
+        })
+    })
 })
 
 module.exports = router
