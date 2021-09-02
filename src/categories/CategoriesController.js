8f5d9d52f0a4095064e1f698efb566d202981314
caiohscruz <caiohscruz@hotmail.com>
2021-09-02 17:54:06 -0300

    Add cadastro de categoria com bd

--- src/categories/CategoriesController.js
+++ src/categories/CategoriesController.js
@@ -1,13 +1,32 @@
 const express = require("express")
 const router = express.Router()
 
+const Category = require("./Category")
+
+const slugify = require("slugify")
+
 router.get("/categories", (req,res) => {
     res.send("teste")
 })
 
 // Form para criar categories
 router.get("/admin/categories/new", (req,res) => {
-    res.render("admin/categories/new.ejs")
+    res.render("admin/categories/newCategory.ejs")
+})
+
+// Salvar categories
+router.post("/categories/save", (req,res) => {
+    var title = req.body.title
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(()=> {
+            res.redirect("/")
+        })
+    }else{
+        res.redirect("admin/categories/newCategory.ejs")
+    }
 })
 
 module.exports = router
