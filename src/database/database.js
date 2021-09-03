356814eacfed40c8e14073dac1adf288ee9c150d
caiohscruz <caiohscruz@hotmail.com>
2021-09-03 16:03:00 -0300

    set timezone brazil

--- src/database/database.js
+++ src/database/database.js
@@ -10,7 +10,8 @@ const connection = new Sequelize(
     process.env.DB_USERNAME, 
     process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    timezone: "-03:00" // BR
 })
 /*
 // passando string de conexao
