7e0f59ecb4700e4631696fa833258e7cd81f84f7
caiohscruz <caiohscruz@hotmail.com>
2021-09-03 18:17:17 -0300

    Add article edition feat

--- src/articles/ArticlesController.js
+++ src/articles/ArticlesController.js
@@ -10,7 +10,6 @@ router.get("/admin/articles", async (req, res) => {
     await Article.findAll({
         include: [{
             model: Category
-            
         }],
         order: [
             ['title', 'ASC']
@@ -54,7 +53,7 @@ router.post("/articles/save", (req, res) => {
 })
 // Route to save a article - end
 
-// Route to delete a category - begin
+// Route to delete a article - begin
 router.post("/articles/delete", (req, res) => {
     var id = req.body.id
     if (id != undefined) {
@@ -73,6 +72,56 @@ router.post("/articles/delete", (req, res) => {
         res.redirect("/admin/articles")
     }
 })
-// Route to delete a category - end
+// Route to delete a article - end
+
+// Route to edit article page - begin
+router.get("/admin/articles/edit/:id", async (req, res) => {
+    var id = req.params.id
+
+    if (!isNaN(id)) {
+        await Article.findByPk(id).then(article => {
+            if (article != undefined) {
+
+                Category.findAll().then(categories => {
+
+                    res.render("admin/articles/edit.ejs", {
+                        article: article,
+                        categories: categories
+                    })
+                })
+            } else {
+                res.redirect("/admin/articles")
+            }
+        }).catch(erro => {
+            res.redirect("/admin/articles")
+        })
+    } else {
+        res.redirect("/admin/articles")
+    }
+})
+// Route to edit article page - end
+
+// Route to update a article - begin
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.update({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles/")
+    })
+})
+// Route to update a article - end
+
 
 module.exports = router
