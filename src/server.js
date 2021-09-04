aa94cf4c04d11056aa88466d4d4b6915c6897c3b
caiohscruz <caiohscruz@hotmail.com>
2021-09-04 12:01:19 -0300

    Add route to an article

--- src/server.js
+++ src/server.js
@@ -1,47 +1,53 @@
-// Setando o express
+// Setting express - begin
 const express = require("express")
-const app     = express()
+const app = express()
 const path = require('path')
+// Setting express - end
 
-// Setando conexão com o banco de dados
+// Importo DB connection - begin
 const connection = require("./database/database")
+// Importo DB connection - end
 
-// Importando os controllers
+// Import controllers - begin
 const CategoriesController = require("./categories/CategoriesController")
 const ArticlesController = require("./articles/ArticlesController")
+// Import controllers - end
 
-// Importando os models
+// Import models - begin
 const Category = require("./categories/Category")
 const Article = require("./articles/Article")
+// Import models - end
 
-// Dizendo para o express que o EJS deve ser usado como View Engine
+// Setting View Engine - begin
 app.set('view engine', 'ejs')
-
-// Para que o express reconheça imagens e CSS - public é o nome do diretorio
-app.use(express.static('public'))
-
 app.set('views', path.join(__dirname, 'views'))
+// Setting View Engine - end
 
-// Para trabalhar com formulários
+// Setting public to static content - begin
+app.use(express.static('public'))
+// Setting public to static content - end
+
+// Settings to use forms - begin
 app.use(express.json())
 app.use(express.urlencoded({
     extended: false
 }))
+// Settings to use forms - end
 
 // Conecction test - begin
 connection
-.authenticate()
-.then( () => {
-    console.log("Conexão feita com o banco de dados")
-})
-.catch((msgErro) => {
-    console.log(msgErro)
-})
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro)
+    })
 // Conecction test - end
 
 // Route to index - begin
-app.get("/", (req,res) =>{
-    Article.findAll().then(articles =>{
+app.get("/", (req, res) => {
+    Article.findAll().then(articles => {
         res.render("index.ejs", {
             articles: articles
         })
@@ -49,14 +55,30 @@ app.get("/", (req,res) =>{
 })
 // Route to index - end
 
-// Route to an article
+// Route to an article- begin
+app.get("/:slug", (req, res) => {
+    var slug = req.params.slug
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }). then (article =>{
+        if(article!=undefined){
+            res.render("")
+        }else{
+            res.redirect("/")
+        }
+    }).catch( erro =>{
+        res.redirect("/")
+    })
+})
+// Route to an article- begin
 
-
-// Integrando os controlers
+// Import controllers routes - begin
 app.use("/", CategoriesController)
 app.use("/", ArticlesController)
+// Import controllers routes - end
 
-
-app.listen(process.env.PORT||8081, ()=> {
+app.listen(process.env.PORT || 8081, () => {
     console.log("Rodando")
 })
